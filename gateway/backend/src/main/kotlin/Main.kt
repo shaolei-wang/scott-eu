5436f22a4a05ef563a96ad553f9b10ad3725796f
Andrew Berezovskyi <andriib@kth.se>
2018-10-22 15:08:57 +0200

    clean up and deserialise JSON on the BE

--- gateway/backend/src/main/kotlin/Main.kt
+++ gateway/backend/src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.zeromq.ZContext
 import org.zeromq.ZMQ
 
@@ -10,25 +13,32 @@ import org.zeromq.ZMQ
  */
 
 const val PULL_PORT = 5557
-const val BIND_ADDR = "tcp://127.0.0.1:$PULL_PORT"
+const val CONNECT_ADDR = "tcp://127.0.0.1:$PULL_PORT"
 
 fun main(args: Array<String>) {
-    println("Pulling from $BIND_ADDR")
+    println("Pulling from $CONNECT_ADDR")
+    val mapper = ObjectMapper().registerKotlinModule()
     ZContext().use { context ->
         // Socket to talk to clients
         val socket = context.createSocket(ZMQ.PULL)
-        socket.connect(BIND_ADDR)
+        socket.connect(CONNECT_ADDR)
 
         while (!Thread.currentThread().isInterrupted) {
             // Block until a message is received
             val reply = socket.recv(0)
 
             // Print the message
-            println("Received: [" + String(reply, ZMQ.CHARSET) + "]")
+            val rcvMessage = String(reply, ZMQ.CHARSET)
+            // Uncomment the line below if you want to see raw JSON
+//            println("Received: '$rcvMessage'")
 
-            // Send a response
-            val response = "Hello, world!"
-            socket.send(response.toByteArray(ZMQ.CHARSET), 0)
+            // show how Jackson can automatically map the JSON to a Kotlin class
+            val msg = mapper.readValue<WorkerMessage>(reply)
+            println("Worker number: ${msg.num}")
+
+            // Can't send a response over
+//            val response = "Hello, world!"
+//            socket.send(response.toByteArray(ZMQ.CHARSET), 0)
         }
     }
 
--- gateway/frontend/main.py
+++ gateway/frontend/main.py
@@ -1,18 +1,32 @@
 import time
 import zmq
+import json
 
 BIND_ADDR = "tcp://127.0.0.1:5557"
 
-def producer():
+def producer(addr):
+    """Send JSON messages via ZMQ.PUSH
+
+    Bind on the given addr and send 20000 messages.
+    """
     context = zmq.Context()
     zmq_socket = context.socket(zmq.PUSH)
-    zmq_socket.bind(BIND_ADDR)
+    zmq_socket.bind(addr)
     # Start your result manager and workers before you start your producers
     for num in range(20000):
         work_message = { 'num' : num }
         zmq_socket.send_json(work_message)
-        print("Sent message: " + work_message)
+        print("Sent message: " + to_json(work_message))
+
+def to_json(msg):
+    """Pretty-print a dict as a JSON string
+
+    Use Unicode and 2-space indents.
+    """
+    return json.dumps(msg, ensure_ascii=False, indent=2)
+
 
 if __name__ == '__main__':
+    # the script was invoked directly from the command line
     print("Pushing to " + BIND_ADDR)
-    producer()
+    producer(BIND_ADDR)
