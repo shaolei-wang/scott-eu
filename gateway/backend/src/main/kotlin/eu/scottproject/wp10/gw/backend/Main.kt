324df6d7dd0b7d9dcc45fc6611f33c426c3f10fd
Andrew Berezovskyi <andriib@kth.se>
2018-11-21 14:30:27 +0100

    Move Gateway code under a proper package

--- gateway/backend/src/main/kotlin/eu/scottproject/wp10/gw/backend/Main.kt
+++ gateway/backend/src/main/kotlin/eu/scottproject/wp10/gw/backend/Main.kt
@@ -1,4 +1,4 @@
-
+package eu.scottproject.wp10.gw.backend
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
